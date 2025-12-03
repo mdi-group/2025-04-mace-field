246ea69550599a1bedfb43e3320db082535a28f8
Bradley Martin <uccabaa@ucl.ac.uk>
2025-12-03 15:52:07 -0500

    Add dielectric training script

---
+++ scripts/Dielectrics/train-dielectric.sh
@@ -0,0 +1,51 @@
+
+torchrun --standalone --nproc_per_node="gpu" ../mace/cli/run_train.py \
+    --name="MACE-dielectric" \
+    --train_file="dielectric-train.xyz" \
+    --valid_file="dielectric-valid.xyz" \
+    --test_file="dielectric-test.xyz" \
+    --E0s="average" \
+    --loss='universal_field' \
+    --energy_weight=10.0 \
+    --forces_weight=0.0 \
+    --stress_weight=0.0 \
+    --polarisation_weight=0.0 \
+    --becs_weight=10.0 \
+    --polarisability_weight=100.0 \
+    --compute_polarisation=True \
+    --compute_becs=True \
+    --compute_polarisability=True \
+    --compute_forces=True \
+    --compute_stress=True \
+    --eval_interval=1 \
+    --error_table='PerAtomRMSEstressvirialsfield' \
+    --model="ScaleShiftFieldMACE" \
+    --interaction_first="RealAgnosticResidualInteractionBlock" \
+    --interaction="RealAgnosticResidualInteractionBlock" \
+    --num_interactions=2 \
+    --correlation=3 \
+    --r_max=5.0 \
+    --max_L=1 \
+    --max_ell=3 \
+    --num_channels=128 \
+    --num_radial_basis=10 \
+    --MLP_irreps="16x0e" \
+    --num_workers=1 \
+    --lr=0.005 \
+    --weight_decay=1e-8 \
+    --ema \
+    --ema_decay=0.995 \
+    --scheduler_patience=10 \
+    --max_num_epochs=1000 \
+    --patience 50 \
+    --amsgrad \
+    --distributed \
+    --device="cuda" \
+    --enable_cueq True \
+    --seed=23 \
+    --default_dtype="float64" \
+    --save_cpu \
+    --batch_size 4 \
+    --valid_batch_size 4 \
+    --restart_latest \
+    --plot=True \
