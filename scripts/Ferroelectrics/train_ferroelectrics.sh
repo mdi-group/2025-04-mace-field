721c67ad6d7e3463edc872bec7b8f151edac9766
Bradley Martin <uccabaa@ucl.ac.uk>
2025-12-03 15:47:32 -0500

    Add ferroelectric training script

---
+++ scripts/Ferroelectrics/train_ferroelectrics.sh
@@ -0,0 +1,51 @@
+
+torchrun --standalone --nproc_per_node=2 ../../mace/cli/run_train.py \
+    --name="MACE-field-ferroelectrics" \
+    --train_file="ferroelectric.xyz" \
+    --valid_fraction=0.10 \
+    --E0s="average" \
+    --loss='universal_field' \
+    --energy_weight=0.1 \
+    --forces_weight=1.0 \
+    --stress_weight=0.0 \
+    --polarisation_weight=1e5 \
+    --becs_weight=0.0 \
+    --polarisability_weight=0.0 \
+    --compute_polarisation=True \
+    --compute_becs=True \
+    --compute_polarisability=True \
+    --compute_forces=True \
+    --compute_stress=True \
+    --eval_interval=1 \
+    --error_table='PerAtomRMSEstressvirialsfield' \
+    --model="ScaleShiftFieldMACE" \
+    --interaction_first="RealAgnosticResidualInteractionBlock" \
+    --interaction="RealAgnosticResidualInteractionBlock" \
+    --num_interactions=2 \
+    --correlation=3 \
+    --r_max=5.0 \
+    --max_L=1 \
+    --max_ell=3 \
+    --num_channels=128 \
+    --num_radial_basis=10 \
+    --MLP_irreps="16x0e" \
+    --num_workers=1 \
+    --lr=0.005 \
+    --weight_decay=1e-8 \
+    --ema \
+    --ema_decay=0.995 \
+    --scheduler_patience=10 \
+    --max_num_epochs=10 \
+    --patience 150 \
+    --amsgrad \
+    --distributed \
+    --device="cuda" \
+    --enable_cueq True \
+    --seed=3 \
+    --default_dtype="float64" \
+    --save_cpu \
+    --batch_size 1 \
+    --valid_batch_size 1 \
+    --restart_latest \
+    --plot=True \
+    --plot_frequency 10 \
